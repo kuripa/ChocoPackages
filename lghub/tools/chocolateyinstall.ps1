ac5f5450870022b1e70b476ccd957f8376c9eb4c
Nick Fuhrmann <n.fuhrmann@protonmail.com>
2021-07-05 12:30:29 -0700

    Edit install.ps1

--- lghub/tools/chocolateyinstall.ps1
+++ lghub/tools/chocolateyinstall.ps1
@@ -15,15 +15,13 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 #$fileLocation = '\\SHARE_LOCATION\to\INSTALLER_FILE'
 # Community Repo: Use official urls for non-redist binaries or redist where total package size is over 200MB
 # Internal/Organization: Download from internal location (internet sources are unreliable)
-$url        = '' # download url, HTTPS preferred
-$url64      = '' # 64bit URL here (HTTPS preferred) or remove - if installer contains both (very rare), use $url
+$url        = 'https://download01.logi.com/web/ftp/pub/techsupport/gaming/lghub_installer.exe' # download url, HTTPS preferred
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
-  fileType      = 'EXE_MSI_OR_MSU' #only one of these: exe, msi, msu
+  fileType      = 'EXE' 
   url           = $url
-  url64bit      = $url64
   #file         = $fileLocation
 
   softwareName  = 'lghub*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
@@ -32,10 +30,8 @@ $packageArgs = @{
   # To determine checksums, you can get that from the original site if provided. 
   # You can also use checksum.exe (choco install checksum) and use it 
   # e.g. checksum -t sha256 -f path\to\file
-  checksum      = ''
+  checksum      = '2cca98cd52e8abf6c6c38af260b7d14689c21db2477dad868034ff2bb1323f62'
   checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
-  checksum64    = ''
-  checksumType64= 'sha256' #default is checksumType
 
   # MSI
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
